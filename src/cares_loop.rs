3adac042724574e9b4eebfc102c99e92be5d8d37
Colm Hickey <colmohici@gmail.com>
2016-08-22 00:41:18 +0100

    licensing

--- src/cares_loop.rs
+++ src/cares_loop.rs
@@ -1,3 +1,16 @@
+/*
+  Copyright 2016 Colm Hickey <colmohici@gmail.com>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 // basically entirely taken from https://github.com/dimbleby/rust-c-ares/blob/master/examples/unix/cares_epoll.rs
 
 use std::sync::{Arc, Mutex};
